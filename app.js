285298cdf07d5f90cb0f4db608e4861bf30d8416
Jim Ntare <jim.ntare@Jims-MacBook-Pro-2.local>
2020-01-14 15:53:28 +0200

    introduced the db dependency

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('app:startup')
 const config = require('config')
 const morgan = require('morgan')
 const helmet = require('helmet')
@@ -14,7 +15,7 @@ console.log('Application Password:' + config.get('mail.password'));
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  console.log('Morgan enabled...')
+  debug('Morgan enabled...')
 }
 
 app.use(express.json());
